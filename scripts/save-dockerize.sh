e60f9e1e50e438a4c870ce94c93f7cee09143e2e
Sosthene Grosset-Janin <educ1.eoex@gmail.com>
2025-12-17 00:52:18 +0100

    Dockerize all services: frontend, backend, database, docker-compose.yml

---
+++ scripts/save-dockerize.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Save and push the Dockerization stage for EOEX Store
+# Usage: ./scripts/save-dockerize.sh
+
+../eoex-store-saves.sh "Dockerize all services: frontend, backend, database, docker-compose.yml"
