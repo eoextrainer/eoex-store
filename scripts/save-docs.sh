7e3a00c954f62d3869899f7f80df2c61dc13b2ce
Sosthene Grosset-Janin <educ1.eoex@gmail.com>
2025-12-17 00:53:24 +0100

    Add documentation: setup, usage, workflow in all major folders

---
+++ scripts/save-docs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Save and push the documentation stage for EOEX Store
+# Usage: ./scripts/save-docs.sh
+
+../eoex-store-saves.sh "Add documentation: setup, usage, workflow in all major folders"
